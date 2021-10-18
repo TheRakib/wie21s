0712209e1fa9a15d7e3367a2958c27082178106d
RakibTest <rakibmedieinstitutet@gmail.com>
2021-10-18 11:46:17 +0200

    js intro

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 
+// https://www.khanacademy.org/computing/computer-programming/programming 
 
+// data structure and algorithm 
 
-// variable deklaration :-
+// variable deklaration // defined variable // definerar man variable :-
 var name;
 
 //  initialize :- 
@@ -66,11 +68,51 @@ for(var index= 0; index< 4 ; index++)
     console.log( list[index] )
     
 }
+// en lika med = : brukar vi tilldela value 
+// två lika med == : jämför med två values 
+// tre lika med === :  Jämför samt kolla data typen 
 
 // jämförelse :När man jämför två values - resultat blir true/false 
 
+   // < 
+   // > 
+   // ==  
+   // ===
+   // !   //not
+   // => 
+   // =< 
+   // !=
+   
+   // lägger man flera vilkor då används de 
+   //  && : and 
+   //  || : or 
 
 
+
+   var age = 11;
+/* 
+   false || true 
+   true && false 
+ */
+
+   // conditonal / vilkorsats
+
+   if(age>19)
+   {
+       console.log("kommer att köras när vilkoret är sant ");
+   }
+   // man kan köra if men varje if körs
+   // else if körs bara om ovanstående vilkoret är false 
+   else if(age>10){
+       console.log("else if körs ")
+   }
+   
+   else {
+       console.log("andra delen kommer att köras ")
+   }
+
+
+   
 
 //print
 console.log(personInfo);  
