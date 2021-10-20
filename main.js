7c6a6e7a837706879c1029ad424f89a6b03a8baf
RakibTest <rakibmedieinstitutet@gmail.com>
2021-10-20 10:46:46 +0200

    var and for loop

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-
+/* 
 // https://www.khanacademy.org/computing/computer-programming/programming 
 
 // data structure and algorithm 
+
+// emcascript 5, 6:let , const istället för var
 
 // variable deklaration // defined variable // definerar man variable :-
 var name;
@@ -16,7 +18,9 @@ var name = "rakib";
 
 var Name =  "rakib"; // name och Name is not the same
 
-//kl. 10.00
+
+
+// web frontend api /browser api 
 
 // data types/datatyper: 
 // text - string 
@@ -48,10 +52,10 @@ var list = [ 5, 14, 15, 24]
 // hur skriver ut values från en lista ??
 
 // indexnummer : börjar från 0. 
-/* console.log(list[0])
+ console.log(list[0])
 console.log(list[1])
 console.log(list[2])
-console.log(list[3]) */
+console.log(list[3])
 
 
 // var i = 1 
@@ -90,10 +94,10 @@ for(var index= 0; index< 4 ; index++)
 
 
    var age = 11;
-/* 
+
    false || true 
    true && false 
- */
+
 
    // conditonal / vilkorsats
 
@@ -106,13 +110,113 @@ for(var index= 0; index< 4 ; index++)
    else if(age>10){
        console.log("else if körs ")
    }
-   
+
    else {
        console.log("andra delen kommer att köras ")
    }
 
 
+   // function/metod: 
+
+   //visa saker och ting/ syntax är js egna keyword till ex. function ,
+   // var, for , if , else ..... 
+
+   // . in i 
+
+   //define 
+   function skrivaUt() {
+       //vad function gör 
+       console.log("Hello Wie21s")
+   }
    
 
-//print
-console.log(personInfo);  
+   // function call, anropar vi funktionen
+   skrivaUt() // skriver ut "hello Wie21s i browsers' console"
+
+   //print
+console.log(personInfo);   */
+
+
+
+/* document.querySelector("h1").textContent;
+
+document.querySelector("p").textContent;
+
+console.log(document.querySelector("p").textContent + document.querySelector("h1").textContent)
+
+console.log(document.querySelector("p").textContent + document.querySelector("h1").textContent)
+//document.write( document.querySelector("h1").textContent)
+*/
+/* var h1 = document.querySelector("h1").textContent;
+var p = document.querySelector("p").textContent;
+
+console.log(h1+p) */
+
+
+// document.querySelector("h1")
+// document.querySelector(".classnamnet")
+// document.querySelector("#idnamnet")
+
+     //.textContent
+     //.innerHTML 
+     //.value   // input fält
+
+
+
+   /*  // ska skapa en p element med hjälp av js
+    var p2= document.createElement("p")   //skapar en element. 
+    // och skriva lite content i p element
+    p2.textContent ="Hello från wie21s";
+    
+    // lägger vi p element i html filen'
+     // steg1: välj nån befientlig element
+     var body = document.querySelector("body")
+     // steg2: append/lägg till p element in i body 
+     body.appendChild(p2)
+
+
+// skapa en h2 tag/element och vill jag lägga h2 in i div element. 
+
+var h2 = document.createElement("h2");
+
+h2.textContent ="hello rubrik 2";
+
+
+var div = document.querySelector("div");
+
+div.appendChild(h2);
+ */
+
+/*
+var li1 = document.querySelector("#li-1").textContent
+var li2 = document.querySelector("#li-2").textContent
+var li3 = document.querySelector("#li-3").textContent
+var li4 = document.querySelector("#li-4").textContent
+var li5 = document.querySelector("#li-5").textContent
+
+
+var li1Converted= Number(li1)
+
+console.log(li1Converted+Number(li2)+Number(li3)+Number(li4)+Number(li5))
+
+*/
+var li = document.querySelectorAll("li");
+console.log(li);
+//for loop.
+var counter = 0
+
+for(var i=0; i<5; i++)
+{
+   console.log(Number(li[i].textContent))
+    counter += Number(li[i].textContent) ;
+   //plusa ihop sifforna 
+
+}
+
+console.log(counter)
+
+// skriva ut sifforna i prislista diven
+var prislista = document.querySelector(".prisLista");
+prislista.textContent = "Totalt blir " + counter;
+
+//kl. 11.00
