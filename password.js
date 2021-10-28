a007696d2c215fee5b5ba741ed45da6e4d8d8395
RakibTest <rakibmedieinstitutet@gmail.com>
2021-10-28 10:01:06 +0200

    password js

---
+++ password.js
@@ -0,0 +1,18 @@
+
+//läsa in data från två input fälten 
+
+ function checkPassword(){
+
+    const password = document.querySelector("#password").value;
+    
+    console.log(password);
+    const repeatedPassword = document.querySelector("#password-repeat").value;
+    console.log(repeatedPassword);
+
+    //Jämför om värderna matchar ?? 
+
+    //kl. 10.00 
+
+ }
+
+// lyssna på btn event
